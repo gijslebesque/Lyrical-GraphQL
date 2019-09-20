c7565349cc3a289f079e5bc89fc18f304b1a8b18
gijslebesque <gijslebesque@gmail.com>
2019-09-20 11:35:32 +0200

    like btn

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
+  onLike(id) {
+    this.props.mutate({
+      variables: {
+        id
+      }
+    });
+  }
+
   showList() {
-    return this.props.lyrics.map(({ content }, i) => {
+    return this.props.lyrics.map(({ content, likes, id }, i) => {
       return (
         <li key={i} className="collection-item">
           {content}
+          <div className="vote-box">
+            <i onClick={e => this.onLike(id)} className="material-icons">
+              thumb_up
+            </i>
+            {likes}
+          </div>
         </li>
       );
     });
@@ -14,5 +30,13 @@ class LyricList extends Component {
     return <ul className="collection">{this.showList()}</ul>;
   }
 }
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
 
-export default LyricList;
+export default graphql(mutation)(LyricList);
