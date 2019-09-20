22508bc0b6163ec329ce601c13ea99e613a7363e
gijslebesque <gijslebesque@gmail.com>
2019-09-20 11:42:50 +0200

    optimistic resp

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -3,10 +3,18 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
-  onLike(id) {
+  onLike(id, likes) {
     this.props.mutate({
       variables: {
         id
+      },
+      optimisticResponse: {
+        __typeName: 'Mutation',
+        likeLyric: {
+          id,
+          __typeName: 'LyricType',
+          likes: likes++
+        }
       }
     });
   }
@@ -17,7 +25,7 @@ class LyricList extends Component {
         <li key={i} className="collection-item">
           {content}
           <div className="vote-box">
-            <i onClick={e => this.onLike(id)} className="material-icons">
+            <i onClick={e => this.onLike(id, likes)} className="material-icons">
               thumb_up
             </i>
             {likes}
